cd433c8fc8377b7b13158afaed06faca4956a162
bo <cbs1006@naver.com>
2024-10-09 16:22:14 +0900

    Feat: 로그인 페이지 추가

---
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.mini_dooray.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().permitAll()
+                )
+
+                .formLogin(formLogin ->
+                        formLogin.loginPage("/login")
+
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/")
+                );
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
