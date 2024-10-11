54ae7f77304994d09e654d1fc002339191e17df3
김희망 <gmlakd990514@naver.com>
2024-10-11 22:21:52 +0900

    fix: 프로젝트 컨트롤러 통합

--- src/main/java/com/nhnacademy/mini_dooray/gateway/project/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/project/controller/ProjectController.java
@@ -1,10 +1,11 @@
 package com.nhnacademy.mini_dooray.gateway.project.controller;
 
 import com.nhnacademy.mini_dooray.gateway.project.adapter.ProjectAdapter;
+import com.nhnacademy.mini_dooray.gateway.project.domain.ProjectCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +24,28 @@ public class ProjectController {
         return mav;
     }
 
+    @GetMapping("/project/create")
+    public String viewCreateProject(@RequestParam(required = false) String memberId, Model model) {
+        model.addAttribute("memberId", memberId);
+        return "projectCreate";
+    }
+
+    @PostMapping("/project/create")
+    public String saveProject(@ModelAttribute ProjectCreateRequest projectCreateRequest, Model model) {
+
+        //TODO: api와 연결 후 실행 시 주석 해제
+        //        projectAdapter.createProject(projectCreateRequest);
+        return "redirect:/"+projectCreateRequest.getMemberId()+"/project";
+    }
+
+    @GetMapping("/project/{projectId}")
+    public ModelAndView viewProjectDetail(@PathVariable String projectId) {
+        ModelAndView mav = new ModelAndView("projectDetail");
+        mav.addObject("projectId", projectId);
+        //TODO: api와 연결 후 실행 시 주석 해제
+//        mav.addObject("project", projectAdapter.getProject(projectId));
+        return mav;
+    }
 
 
 }
