3b7e448833258971f57b3103630a7f818c5f2e5e
김희망 <gmlakd990514@naver.com>
2024-10-12 18:26:23 +0900

    feat: 프로젝트 끝 , milestone 시작

---
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/milestone/controller/MilestoneController.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.mini_dooray.gateway.milestone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MilestoneController {
+
+    @GetMapping("/project/{projectId}/task/{taskId}/milestone")
+    public String viewMilestone(@PathVariable String projectId, @PathVariable String taskId) {
+        
+    }
+}
