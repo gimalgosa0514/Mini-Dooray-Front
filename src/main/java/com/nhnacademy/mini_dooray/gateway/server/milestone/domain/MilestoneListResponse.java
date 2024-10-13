6fd5e2ccb3a61a8b960b8a6961df94824af8ec84
bo <cbs1006@naver.com>
2024-10-13 22:47:09 +0900

    feat: milestone 추가

--- src/main/java/com/nhnacademy/mini_dooray/gateway/server/milestone/domain/MilestoneListResponse.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/server/milestone/domain/MilestoneListResponse.java
@@ -4,10 +4,14 @@ package com.nhnacademy.mini_dooray.gateway.server.milestone.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 public class MilestoneListResponse {
 
     private long milestoneId;
     private String milestoneName;
+    private LocalDateTime milestoneStartline;
+    private LocalDateTime milestoneDeadline;
 }
